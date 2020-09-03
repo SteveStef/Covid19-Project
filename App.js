cb27329c155ec21173b6e60cc5742ad16de7e0c6
Stephen Stefanatos <69201849+SteveStef@users.noreply.github.com>
2020-09-03 13:49:40 -0400

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+//import Index from './components/index'
+import Covid from './components/Recipe'
+function App() {
+  return (
+    <div className="App">
+      <Covid />
+    </div>
+  );
+}
+
+export default App;
